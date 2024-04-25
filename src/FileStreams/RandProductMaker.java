f75552edefb616ef1923e84c8a38d1d2d5880bf3
Teklemya <Teklemya@mail.uc.edu>
2024-04-25 18:56:35 -0400

    Initial Commit

---
+++ src/FileStreams/RandProductMaker.java
@@ -0,0 +1,210 @@
+package FileStreams;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class RandProductMaker extends JFrame {
+    private BorderLayout mainLayout = new BorderLayout(0,10);
+    private JPanel mainPnl = new JPanel(mainLayout);
+    private JPanel counterPnl;
+    private JLabel counterLbl;
+    private int count = 0;
+
+    private JPanel addItemPnl;
+    private JLabel itemNumLbl;
+    private JTextField itemNumFld;
+    private JLabel itemNameLbl;
+    private JTextField itemNameFld;
+    private JLabel itemDescLbl;
+    private JTextField itemDescFld;
+    private JLabel itemCostLbl;
+    private JTextField itemCostFld;
+
+    private JPanel cmdPnl;
+
+    private String newItemID;
+    private String newItemName;
+    private String newItemDesc;
+    private String newItemCost;
+
+    private ArrayList<Product> productList = new ArrayList<>();
+
+    private File workingDirectory = new File(System.getProperty("user.dir"));
+    private Path path = Paths.get(workingDirectory.getPath() + "/Src/FileStreams/ProductData.bin");
+
+    public static void main(String[] args) {new RandProductMaker();}
+
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+    public RandProductMaker(){
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Product Creator");
+
+
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(path.toFile())))
+        {
+            productList = (ArrayList<Product>) in.readObject();
+            count = productList.size();
+        }
+        catch (ClassNotFoundException p)
+        {
+            p.printStackTrace();
+        }
+        catch (IOException p)
+        {
+            p.printStackTrace();
+        }
+
+        add(mainPnl);
+
+        generateCounterPnl();
+        mainPnl.add(counterPnl, BorderLayout.NORTH);
+
+        generateAddItemPnl();
+        mainPnl.add(addItemPnl, BorderLayout.CENTER);
+
+        generateCmdPnl();
+        mainPnl.add(cmdPnl, BorderLayout.SOUTH);
+
+        pack();
+        setSize((int)(.5*screenSize.width),(int)(.2*screenSize.height));
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    private void generateCounterPnl(){
+        Border margin = new EmptyBorder(10,10,5,10);
+        counterPnl = new JPanel(new GridLayout(1,1));
+
+        counterLbl = new JLabel("Item Counter: " + count);
+        counterLbl.setHorizontalAlignment(SwingConstants.CENTER);
+        counterPnl.setBorder(margin);
+        counterPnl.add(counterLbl);
+    }
+
+    private void generateAddItemPnl(){
+        Border margin = new EmptyBorder(10,10,5,10);
+        addItemPnl = new JPanel(new GridLayout(4,2));
+        addItemPnl.setBorder(margin);
+
+        itemNumLbl = new JLabel("Item ID # (6 Digits):");
+        itemNumLbl.setHorizontalAlignment(SwingConstants.TRAILING);
+        itemNumFld= new JTextField(6);
+        itemNumFld.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                if (itemNumFld.getText().length() >= 6 ) // limit textfield to 3 characters
+                    e.consume();
+            }
+        });
+
+        addItemPnl.add(itemNumLbl);
+        addItemPnl.add(itemNumFld);
+
+        itemNameLbl = new JLabel("Item Name (35 Character Max): ");
+        itemNameLbl.setHorizontalAlignment(SwingConstants.TRAILING);
+        itemNameFld = new JTextField(35);
+        itemNameFld.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                if (itemNameFld.getText().length() >= 35 ) // limit textfield to 3 characters
+                    e.consume();
+            }
+        });
+        addItemPnl.add(itemNameLbl);
+        addItemPnl.add(itemNameFld);
+
+        itemDescLbl = new JLabel("Item Description (75 Character Max):");
+        itemDescLbl.setHorizontalAlignment(SwingConstants.TRAILING);
+        itemDescFld = new JTextField(75);
+        itemDescFld.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                if (itemDescFld.getText().length() >= 75) // limit textfield to 3 characters
+                    e.consume();
+            }
+        });
+        addItemPnl.add(itemDescLbl);
+        addItemPnl.add(itemDescFld);
+
+        itemCostLbl = new JLabel("Cost (9 Character Max):");
+        itemCostLbl.setHorizontalAlignment(SwingConstants.TRAILING);
+        itemCostFld = new JTextField();
+        itemCostFld.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                if (itemCostFld.getText().length() >= 9) // limit textfield to 3 characters
+                    e.consume();
+            }
+        });
+        addItemPnl.add(itemCostLbl);
+        addItemPnl.add(itemCostFld);
+
+    }
+
+    private void generateCmdPnl(){
+        cmdPnl = new JPanel(new GridLayout(1,3));
+        JButton addBtn = new JButton("Add a Record");
+        JButton removeBtn = new JButton("Remove ALL Records");
+        JButton quitBtn = new JButton("Quit");
+
+        addBtn.addActionListener(e ->{
+            if(itemNumFld.getText().length() > 0 && itemNameFld.getText().length() > 0 && itemDescFld.getText().length() > 0 && itemCostFld.getText().length() > 0){
+                newItemID = itemNumFld.getText();
+                newItemName = itemNameFld.getText();
+                newItemDesc = itemDescFld.getText();
+                newItemCost =  itemCostFld.getText();
+
+
+                Product currentProd = new Product(newItemID,newItemName,newItemDesc, Double.valueOf(newItemCost));
+                productList.add(currentProd);
+
+
+                try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(path.toFile())))
+                {
+                    out.writeObject(productList);
+                }
+                catch (IOException k)
+                {
+                    k.printStackTrace();
+                }
+                count = productList.size();
+                counterLbl.setText("Item Counter: " + count);
+
+                itemNumFld.setText("");
+                itemNameFld.setText("");
+                itemDescFld.setText("");
+                itemCostFld.setText("");
+            } else{
+                JOptionPane.showMessageDialog(addItemPnl, "One or more field(s) are blank!", "Uh-Oh! Blank Field Alert", JOptionPane.ERROR_MESSAGE);
+            }
+        });
+        cmdPnl.add(addBtn);
+
+        removeBtn.addActionListener(e -> {
+            for(int i = productList.size()-1; i > -1; i--) {
+                productList.remove(i);
+            }
+
+            try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(path.toFile())))
+            {
+                out.writeObject(productList);
+            }
+            catch (IOException k)
+            {
+                k.printStackTrace();
+            }
+
+            count = productList.size();
+            counterLbl.setText("Item Counter: " + count);
+        });
+        cmdPnl.add(removeBtn);
+
+        quitBtn.addActionListener(e -> System.exit(0));
+        cmdPnl.add(quitBtn);
+    }
+}
---
+++ src/FileStreams/RandProductSearch.java
@@ -0,0 +1,126 @@
+package FileStreams;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class RandProductSearch extends JFrame {
+    private ArrayList<Product> productList = new ArrayList<>();
+    private File workingDirectory = new File(System.getProperty("user.dir"));
+    private Path path = Paths.get(workingDirectory.getPath() + "/Src/FileStreams/ProductData.bin");
+    private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    private JPanel searchPnl, displayPnl, cmdPnl;
+    private JLabel searchLbl;
+    private JTextField searchFld;
+    private JTextArea displayArea = new JTextArea();
+    private JScrollPane scroller = new JScrollPane(displayArea);
+    private String searchStr = "";
+
+
+
+    public static void main(String[] args) {
+        RandProductSearch runner = new RandProductSearch();
+    }
+
+    public RandProductSearch(){
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(path.toFile())))
+        {
+            productList = (ArrayList<Product>) in.readObject();
+            for(Product k : productList){
+                k.setName(k.getName().trim());
+                k.setDescription(k.getDescription().trim());
+            }
+        }
+        catch (ClassNotFoundException p)
+        {
+            p.printStackTrace();
+        }
+        catch (IOException p)
+        {
+            p.printStackTrace();
+        }
+
+        JPanel mainPnl = new JPanel(new BorderLayout());
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Product Search");
+
+        add(mainPnl);
+
+        generateSearchPnl();
+        mainPnl.add(searchPnl, BorderLayout.NORTH);
+
+        generateDisplayPnl();
+        mainPnl.add(displayPnl);
+
+        generateCmdPnl();
+        mainPnl.add(cmdPnl, BorderLayout.SOUTH);
+
+        pack();
+        setSize((int)(.65*screenSize.width),(int)(.5*screenSize.height));
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    private void generateSearchPnl(){
+        searchPnl = new JPanel();
+        searchLbl = new JLabel("Search:");
+        searchLbl.setHorizontalAlignment(SwingConstants.CENTER);
+
+        searchPnl.add(searchLbl);
+
+        searchFld = new JTextField(15);
+        searchPnl.add(searchFld);
+        searchPnl.setPreferredSize(new Dimension(100,45));
+        searchFld.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e){
+
+                displayArea.setText(String.format("%-35s%-75s%-9s","Name","Description","Price") + "\n"+String.format("%-119s","=========================================================================================================================================\n"));
+
+                if(e.getKeyCode() == 8 && !searchStr.isEmpty()){
+                    searchStr = searchStr.substring(0,searchStr.length()-1);
+                } else if(e.getKeyCode() != 8) {
+                    searchStr += e.getKeyChar();
+                }
+
+                for(Product p: productList){
+                    if(p.getName().toLowerCase().contains(searchStr.toLowerCase()) && !searchStr.isBlank()){
+                        //displayArea.append(p.getName()+"\t"+p.getDescription()+"\t$"+p.getCost()+"\n");
+                        displayArea.append(String.format("%-35s%-76s%-9.2f",p.getName(),p.getDescription(),p.getCost())+"\n");
+                    }
+                }
+
+            }
+        });
+
+    }
+    private void generateDisplayPnl(){
+        displayPnl = new JPanel(new GridLayout(1,1));
+        displayArea.setFont(new Font("Courier New", Font.PLAIN, 14));
+        displayArea.setEditable(false);
+        displayArea.setMargin(new Insets(7,5,0,0));
+        displayArea.setText(String.format("%-35s%-75s%-9s","Name","Description","Price") + "\n"+String.format("%-119s","=========================================================================================================================================\n"));
+
+
+
+        displayPnl.add(scroller);
+    }
+    private void generateCmdPnl(){
+        cmdPnl = new JPanel(new GridLayout(1,1));
+        JButton quitBtn = new JButton("Quit");
+        cmdPnl.setPreferredSize(new Dimension(100,40));
+        quitBtn.setFont(new Font("Helvetica",Font.PLAIN,20));
+        quitBtn.addActionListener(e -> System.exit(0));
+        cmdPnl.add(quitBtn);
+    }
+
+}
